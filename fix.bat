38cadbb6cd787481082bfc91bc6012e3691e4d64
Derek Morton <derek.morton25@gmail.com>
2012-06-12 04:29:08 -0500

    More tweaks to general program flow.

--- fix.bat
+++ fix.bat
@@ -7,6 +7,8 @@ echo This tool helps remove Sirefef.P
 echo.
 echo This variant can be identifed by an "n" file
 echo found in Process Explorer in DLL view for explorer.exe
+echo.
+pause
 
 reg query HKLM\Software\Classes\CLSID\{F3130CDB-AA52-4C3A-AB32-85FFC23AF9C1}\InprocServer32 | find /i "wbemess.dll" > null
 if %errorlevel% == 0 goto not_infected
@@ -17,9 +19,9 @@ cls
 echo.
 echo Sirefef.P does not seem to be present on this machine
 echo.
-set /p question= Are you sure you want to continue? (Y or N)
-if %question% == Y goto repair
-if %question% == y goto repair
+set /p prompt= Are you sure you want to continue? (Y or N) 
+if %prompt% == Y goto repair
+if %prompt% == y goto repair
 goto quiting
 
 :infected
@@ -27,17 +29,15 @@ cls
 echo.
 echo Sirefef.P infection found on this machine
 echo.
-set /p question2= Ready to start repair? (Y or N)
-if %question2% == Y goto repair
-if %question2% == y goto repiar
-goto quiting
+pause
+goto repair
 
 :repair
 cls
 echo.
 echo Doing repair
-reg add HKLM\Software\Classes\CLSID\{F3130CDB-AA52-4C3A-AB32-85FFC23AF9C1}\InprocServer32 /ve /t reg_expand_sz /d ^%systemroot^%\system32\wbem\wbemess.dll
-reg delete HKCU\Software\Classes\clsid\{42aedc87-2188-41fd-b9a3-0c966feabec1}
+reg add HKLM\Software\Classes\CLSID\{F3130CDB-AA52-4C3A-AB32-85FFC23AF9C1}\InprocServer32 /ve /t reg_expand_sz /d ^%systemroot^%\system32\wbem\wbemess.dll /f
+reg delete HKCU\Software\Classes\clsid\{42aedc87-2188-41fd-b9a3-0c966feabec1} /f
 goto done
 
 :done
@@ -46,8 +46,10 @@ echo.
 echo Repair has been completed
 echo.
 echo Reboot your PC to complete the repair
+echo.
 
 :quiting
 cls
 echo.
 echo Repair Cancelled.
+echo.
