23a4ce08e926701e9169ff470a980f0c5f881c4d
Derek Morton <derek.morton25@gmail.com>
2012-06-22 02:01:48 -0500

    Fixed infected/not infected verifcation check

--- postfix.bat
+++ postfix.bat
@@ -6,9 +6,10 @@ echo Please wait.
 echo.
 
 reg query HKLM\Software\Classes\CLSID\{F3130CDB-AA52-4C3A-AB32-85FFC23AF9C1}\InprocServer32 /ve | find /i "wbemess.dll" > null
-if %errorlevel% == 0 goto not_infected
+if %errorlevel% == 0 goto check
 if %errorlevel% == 1 goto infected
 
+:check
 reg query HKCU\Software\Classes\clsid\{42aedc87-2188-41fd-b9a3-0c966feabec1}
 if %errorlevel% == 0 goto not_infected
 if %errorlevel% == 1 goto infected
